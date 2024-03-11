dc9a3522d13f839e2a6edfc8586e5e07631a839f
Anamul029 <anamhaque.5101@gmail.com>
2024-03-11 22:43:41 +0600

    update login part

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,5 +1,28 @@
 const Login = () => {
-  return <div>Login Page</div>;
+  return <div>
+    {/* Login Page */}
+    <div className="w-4/5 container mx-auto flex justify-center">
+      <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+        <form className="card-body">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input type="email" placeholder="email" className="input input-bordered" required />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input type="password" placeholder="password" className="input input-bordered" required />
+          </div>
+          <div className="form-control mt-6">
+            <button className="btn btn-primary">Login</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>;
 };
 
 export default Login;
