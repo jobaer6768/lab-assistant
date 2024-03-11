a771cfaf95ab8cf4d9edfcebe3c4ec3f8a9c456c
Anamul029 <anamhaque.5101@gmail.com>
2024-03-11 22:25:00 +0600

    update this banner section

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -1,8 +1,19 @@
 const Banner = () => {
   return (
     <div>
-      This Banner
-      <h3>After deployment</h3>
+      {/* This Banner section */}
+      <div className="w-4/5 container mx-auto my-8">
+        <div className="hero min-h-screen " style={{ backgroundImage: 'url(/src/assets/images/banner.webp)' }}>
+          <div className="hero-overlay bg-opacity-60"></div>
+          <div className="hero-content text-center text-neutral-content">
+            <div className="max-w-md">
+              <h1 className="mb-5 text-5xl font-bold"> Lab Assistant </h1>
+              <p className="mb-5">Track your lab instrument</p>
+              <button className="btn btn-primary">Get Started</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,8 @@ import Card from "../../components/shared/Card";
 import CardCollection from "../../components/shared/CardCollection";
 import Footer from "../../components/shared/Footer";
 import Navbar from "../../components/shared/Navbar";
+import Login from "../Login/Login";
+import Register from "../Register/Register";
 import Banner from "./Banner";
 
 const Home = () => {
@@ -11,6 +13,8 @@ const Home = () => {
       <Banner></Banner>
       <CardCollection></CardCollection>
       <Card></Card>
+      <Login></Login>
+      <Register></Register>
       <Footer></Footer>
     </div>
   );
