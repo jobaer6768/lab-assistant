bcfcbe3228f95e693697d57c0e9ed8205eb2e25b
jobaer6768 <jobaer9898@gmail.com>
2024-03-16 02:10:16 +0600

    working with local database

--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-  baseURL: "https://http://localhost:8000",
+  baseURL: "http://localhost:8000",
 });
 
 // Add a request interceptor to include the token in the headers
