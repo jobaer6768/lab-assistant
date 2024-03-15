cb1f280f10593c0300fa5991bc418b8549137b39
jobaer6768 <jobaer9898@gmail.com>
2024-03-15 22:16:59 +0600

    added Add Device Page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,6 @@
+import Card from "../../components/shared/Card";
 import Footer from "../../components/shared/Footer";
+import AddDevice from "../AddDevice/AddDevice";
 import Lab from "../Lab/Lab";
 import Banner from "./Banner";
 
@@ -7,6 +9,8 @@ const Home = () => {
     <div>
       <Banner></Banner>
       <Lab></Lab>
+      <Card></Card>
+      <AddDevice></AddDevice>
       <Footer></Footer>
     </div>
   );
