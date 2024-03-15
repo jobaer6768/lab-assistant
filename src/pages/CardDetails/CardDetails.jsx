11a206e0a2a6397f65905d0c0f7986830fd0af51
HIMU005 <hashanuzzaman99@gamil.com>
2024-03-16 03:43:22 +0600

    add style in CardDetails page

--- src/pages/CardDetails/CardDetails.jsx
+++ src/pages/CardDetails/CardDetails.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import useAxiosPublic from "../../hooks/useAxiosPublic";
+import imgg from '../../assets/images/Computer.jpg'
 
 const CardDetails = () => {
   const { Id } = useParams(); // Use "Id" instead of "_id"
@@ -61,14 +62,45 @@ const CardDetails = () => {
 
   // Render device details once fetched
   return (
-    <div>
-      {/* Display details of the selected device */}
-      <h1>Card Details for Device ID: {Id}</h1>
-      {/* Render device details */}
-      {/* Example: */}
-      <p>Device Name: {deviceName}</p>
-      <p>Brand: {brand}</p>
-      {/* Add more fields as needed */}
+    <div className="flex">
+      {/* left part  */}
+      <div className="grow">
+        <img src={imgg} alt="" />
+        <p className="capitalize text-lg">deviceName: {deviceName}</p>
+        <p className="capitalize text-lg">brand: {brand}</p>
+        <p className="capitalize text-lg">model: {model}</p>
+        <p className="capitalize text-lg">image: {image}</p>
+        <p className="capitalize text-lg">processor: {processor}</p>
+        <p className="capitalize text-lg">processorBrand: {processorBrand}</p>
+        <p className="capitalize text-lg">processorType: {processorType}</p>
+        <p className="capitalize text-lg">processorModel: {processorModel}</p>
+        <p className="capitalize text-lg">processorBaseFrequency: {processorBaseFrequency}</p>
+        <p className="capitalize text-lg">processorCore: {processorCore}</p>
+        <p className="capitalize text-lg">processorThread: {processorThread}</p>
+        <p className="capitalize text-lg">generation: {generation}</p>
+        <p className="capitalize text-lg">RAM: {RAM}</p>
+        <p className="capitalize text-lg">installedRAMDetails: {installedRAMDetails}</p>
+        <p className="capitalize text-lg">RAMType: {RAMType}</p>
+        <p className="capitalize text-lg">RAMBus: {RAMBus}</p>
+        <p className="capitalize text-lg">totalRAMSlot: {totalRAMSlot}</p>
+        <p className="capitalize text-lg">emptyRAMSlot: {emptyRAMSlot}</p>
+        <p className="capitalize text-lg">storage: {storage}</p>
+        <p className="capitalize text-lg">installedHDDType: {installedHDDType}</p>
+        <p className="capitalize text-lg">installedSSDType: {installedSSDType}</p>
+        <p className="capitalize text-lg">speaker: {speaker}</p>
+        <p className="capitalize text-lg">microphone: {microphone}</p>
+        <p className="capitalize text-lg">webCam: {webCam}</p>
+        <p className="capitalize text-lg">keyboard: {keyboard}</p>
+        <p className="capitalize text-lg">mouse: {mouse}</p>
+        <p className="capitalize text-lg">operatingSystem: {operatingSystem}</p>
+        
+    
+
+      </div>
+      {/* assign part  */}
+      <div className="bg-blue-400 text-white p-4">
+        <h1 className="text-4xl font-bold">Assign To</h1>
+      </div>
     </div>
   );
 };
