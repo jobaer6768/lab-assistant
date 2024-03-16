409475c4aa8ad476faedd8b34085298120e3d575
HIMU005 <hashanuzzaman99@gamil.com>
2024-03-16 12:24:41 +0600

    AddDevice page design done

--- src/pages/AddDevice/AddDevice.jsx
+++ src/pages/AddDevice/AddDevice.jsx
@@ -1,5 +1,182 @@
 const AddDevice = () => {
-  return <div>Add Device</div>;
-};
+  return <div className="max-w-screen-lg mx-auto grid grid-cols-4 gap-3 md:grid-cols-3 sm:grid-cols-2">
+
+    <label className="form-control w-full max-w-xs">
+      <div className="label">
+        <span className="label-text">deviceName</span>
+      </div>
+      <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+
+    </label>
+    <label className="form-control w-full max-w-xs">
+      <div className="label">
+        <span className="label-text">brand</span>
+      </div>
+      <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+
+    </label>
+
+    <label className="form-control w-full max-w-xs">
+      <div className="label">
+        <span className="label-text">model</span>
+      </div>
+      <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+    </label>
+
+    <label className="form-control w-full max-w-xs">
+      <div className="label">
+        <span className="label-text">processorBrand</span>
+      </div>
+      <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+
+    </label>
+
+    <label className="form-control w-full max-w-xs">
+      <div className="label">
+        <span className="label-text">processorType</span>
+      </div>
+      <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+
+    </label>
+    <label className="form-control w-full max-w-xs">
+      <div className="label">
+        <span className="label-text">Generation</span>
+      </div>
+      <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+
+    </label>
+    <label className="form-control w-full max-w-xs">
+      <div className="label">
+        <span className="label-text">processorModel</span>
+      </div>
+      <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+
+    </label>
+    <label className="form-control w-full max-w-xs">
+      <div className="label">
+        <span className="label-text">processorBaseFrequency</span>
+      </div>
+      <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+
+    </label>
+    <label className="form-control w-full max-w-xs">
+      <div className="label">
+        <span className="label-text">processorCore</span>
+      </div>
+      <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+
+    </label>
+    <label className="form-control w-full max-w-xs">
+      <div className="label">
+        <span className="label-text">processorThread</span>
+      </div>
+      <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+    </label>
+
+    <label className="form-control w-full max-w-xs">
+      <div className="label">
+        <span className="label-text">RAM</span>
+      </div>
+      <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+
+    </label>
+    <label className="form-control w-full max-w-xs">
+      <div className="label">
+        <span className="label-text">installedRAMDetails</span>
+      </div>
+      <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+
+    </label>
+    <label className="form-control w-full max-w-xs">
+      <div className="label">
+        <span className="label-text">RAMType</span>
+      </div>
+      <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+
+    </label>
+    <label className="form-control w-full max-w-xs">
+      <div className="label">
+        <span className="label-text">RAMBus (MHz)</span>
+      </div>
+      <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+
+    </label>
+    <label className="form-control w-full max-w-xs">
+      <div className="label">
+        <span className="label-text">total RAM Slot</span>
+      </div>
+      <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+
+    </label>
+    <label className="form-control w-full max-w-xs">
+      <div className="label">
+        <span className="label-text">Empty/Expansion RAM Slot</span>
+      </div>
+      <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+
+    </label>
+    <label className="form-control w-full max-w-xs">
+      <div className="label">
+        <span className="label-text">storage</span>
+      </div>
+      <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+
+    </label>
+    <label className="form-control w-full max-w-xs">
+      <div className="label">
+        <span className="label-text">installedHDDType</span>
+      </div>
+      <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+
+    </label>
+    <label className="form-control w-full max-w-xs">
+      <div className="label">
+        <span className="label-text">installedSSDType</span>
+      </div>
+      <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+
+    </label>
+    <label className="form-control w-full max-w-xs">
+      <div className="label">
+        <span className="label-text">Speaker</span>
+      </div>
+      <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+    </label>
+
+    <label className="form-control w-full max-w-xs">
+      <div className="label">
+        <span className="label-text">Microphone</span>
+      </div>
+      <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+    </label>
+    <label className="form-control w-full max-w-xs">
+      <div className="label">
+        <span className="label-text">WebCam</span>
+      </div>
+      <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+    </label>
+    <label className="form-control w-full max-w-xs">
+      <div className="label">
+        <span className="label-text">KeyBoard</span>
+      </div>
+      <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+    </label>
+    <label className="form-control w-full max-w-xs">
+      <div className="label">
+        <span className="label-text">Mouse</span>
+      </div>
+      <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+    </label>
+
+    <label className="form-control w-full max-w-xs">
+      <div className="label">
+        <span className="label-text">Operating System</span>
+      </div>
+      <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+
+    </label>
+    <button className="btn btn-success my-auto">Add Device</button>
+  </div>;
+}
 
 export default AddDevice;
